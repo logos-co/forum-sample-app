8b47fdf9ae673c5108e41c43c3df92c64a84659d
jzaki <james.zaki@proton.me>
2026-06-25 18:27:08 +0100

    broadcast module stub

--- bcast-module/flake.nix
+++ bcast-module/flake.nix
@@ -1,14 +1,12 @@
 {
-  description = "Logos ui_qml module (C++ backend + QML view) — replace with your description";
+  description = "Minimal Logos Module - Example using logos-module-builder";
 
   inputs = {
     logos-module-builder.url = "github:logos-co/logos-module-builder";
-    # Add core module dependencies as inputs (must match metadata.json "dependencies"), e.g.:
-    # some_module.url = "github:logos-co/logos-some-module";
   };
 
   outputs = inputs@{ logos-module-builder, ... }:
-    logos-module-builder.lib.mkLogosQmlModule {
+    logos-module-builder.lib.mkLogosModule {
       src = ./.;
       configFile = ./metadata.json;
       flakeInputs = inputs;
